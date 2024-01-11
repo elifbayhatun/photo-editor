f283a414007873079b3251ea32b5d75a81a8629e
elifbayhatun <bayhatunelif@gmail.com>
2024-01-11 17:56:35 +0300

    first commit

---
+++ photo-editor/src/SidebarItem.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const SidebarItem = ({ name, active, handleClick }) => {
+  return (
+    <div>
+      <button
+        className={`sidebar-item ${active ? "active" : ""}`}
+        onClick={handleClick}
+      >
+        {" "}
+        {name}
+      </button>
+    </div>
+  );
+};
+
+export default SidebarItem;
